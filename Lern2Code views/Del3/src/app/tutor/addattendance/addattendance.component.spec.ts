99dfcc1fad8f28c636da25b44caab552ff7ebc53
Vrajesh Patel <u18102672@tuks.co.za>
2021-06-14 10:14:57 +0200

    Attendance

---
+++ Lern2Code views/Del3/src/app/tutor/addattendance/addattendance.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddattendanceComponent } from './addattendance.component';
+
+describe('AddattendanceComponent', () => {
+  let component: AddattendanceComponent;
+  let fixture: ComponentFixture<AddattendanceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddattendanceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddattendanceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
